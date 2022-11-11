41bf999c63ad3ee4cacd129fc4524064f69c0889
Katie Wilson <kmcwilson4@gmail.com>
2022-11-10 21:46:30 -0500

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const path= require('path');
 const api = require('./routes/index');
 
-const PORT= process.env.port || 5000;
+const PORT= process.env.PORT || 3001;
 
 const app = express();
 
